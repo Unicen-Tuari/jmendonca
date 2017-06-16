67bad06be6d94fc523095fcb039468b27c924483
juanma mendonça <juan86x@gmail.com>
2017-06-16 20:04:57 -0300

    Update route.php

--- route.php
+++ route.php
@@ -14,33 +14,5 @@ function parseUrl($url){
 $controller = new controllerMascota();
 $controller = new controllerAdmin();
 $controller = new controllerForm();
-/*aca copie un pedazo de ejemplo que hizo nacho en clase del route.php
-----------------------------------
-if($_GET[ConfigApp::$ACTION] == ''){
-  $controller->mostrarTareas();
-}
-else{
-  $datos = parseUrl($_GET[ConfigApp::$ACTION]);
-  if($datos[ConfigApp::$RESOURCE] === ConfigApp::$RESOURCE_TASK) {
-    switch ($datos[ConfigApp::$ACTION] ) {
-      case ConfigApp::$ACTION_DELETE:
-        $controller->BorrarTarea($datos[ConfigApp::$PARAMETERS]);
-        break;
-      case ConfigApp::$ACTION_ADD:
-        $controller->InsertarTarea();
-        break;
-      case ConfigApp::$ACTION_VIEW:
-        $controller->mostrarTareaDetalle($datos[ConfigApp::$PARAMETERS]);
-        break;
-      case ConfigApp::$ACTION_FINISHED:
-        $controller->TerminarTarea($datos[ConfigApp::$PARAMETERS]);
-        break;
-      default:
-        $controller->mostrarTareas(["Error: Acción inválida"]);
-        break;
-      }
-  }
-}
---------------------------------------------
-*/
+
  ?>
